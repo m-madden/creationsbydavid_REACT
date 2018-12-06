002ddf1dfebb148491ae671b13b18c98c82646fc
m-madden <mmadde768@gmail.com>
2018-12-06 16:47:13 -0600

    Added Content area for dynamic content from posts

--- src/components/content.js
+++ src/components/content.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { CategoriesContext } from '../Context';
+import { Categories, Category } from '../providers';
+import { ContentNav, Section } from '../content';
 
 export const Content = (props) => {
 	return(
-		<div>Content</div>
+		<div className="content">
+		<Categories>
+			<ContentNav/>
+			<CategoriesContext.Consumer>
+				{({active_category}) => (
+					<Category active_category={active_category}>
+						<Section/>
+					</Category>
+				)}
+			</CategoriesContext.Consumer>
+		</Categories>
+		</div>
 	)
 }
