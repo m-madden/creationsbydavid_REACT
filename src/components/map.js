e14333fa878e7a39224bdc8da060e30618f6b91e
m-madden <mmadde768@gmail.com>
2019-02-08 09:58:25 -0600

    Added infoWindow to mapIcon

--- src/components/map.js
+++ src/components/map.js
@@ -9,20 +9,34 @@ export class Map extends Component {
 	}
 
 	componentDidMount = () => {
+		let infoWindowContent = "<h4>Robin's Gallery</h4>" +
+			"<p>Art, Gifts, Home D&eacute;cor, Custom Framing</p>" +
+			"<p class='small'>2607 Lebanon Pike, Nashville, TN 37214</p>" +
+			"<p class='small'>615-885-1694</p>" +
+			"<a class='x-small' href='http://robinsgallery.com'>robinsgallery.com</a>";
+
+		let infowindow = new google.maps.InfoWindow({
+			content: infoWindowContent
+		});
+
 		let map = new google.maps.Map(this.mapRef.current, {
 			zoom: 11,
 			gestureHandling: "greedy",
-			clickableIcons: false,
 			center: {
 				lat: 36.1696029,
 				lng: -86.6751029
 			}
 		})
-		return new google.maps.Marker({
+
+		let marker = new google.maps.Marker({
 			position: new google.maps.LatLng(36.1696029, -86.6751029),
 			map,
 			icon: require("../images/mapIcon.png")
 		})
+
+		marker.addListener('click', function() {
+			infowindow.open(map, marker)
+		});
 	}
 
 	render() {
