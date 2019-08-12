c5c7e1680539dcd8d94eff2015822653360e2d87
m-madden <mmadde768@gmail.com>
2019-08-12 12:49:33 -0500

    updated react and implemented recaptcha on contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Input, TextArea, LargeButton } from './forms';
-import { SuccessToast } from './';
-import { useContact } from './use-contact';
+import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import { Input, TextArea, LargeButton } from "./forms";
+import { SuccessToast } from "./";
+import { useContact } from "./use-contact";
 
 export const Contact = () => {
-
 	const initialValues = {
 		subject: "",
 		email: "",
@@ -12,30 +12,83 @@ export const Contact = () => {
 		name: "",
 		success: false,
 		valid_email: null
-	}
+	};
 
-	const [ values, onChange, validateEmail, submitForm ] = useContact(initialValues);
-
-	return(
+	const [
+		validateRecaptcha,
+		recaptchaStatus,
+		values,
+		onChange,
+		validateEmail,
+		submitForm
+	] = useContact(initialValues);
+	return (
 		<div className="static_content">
 			<div className="aside">
-				<aside>David's work is available for purchase at&nbsp;<a href="http://robinsgallery.com">Robin's&nbsp;Gallery</a></aside>
+				<aside>
+					David's work is available for purchase at&nbsp;
+					<a href="http://robinsgallery.com">Robin's&nbsp;Gallery</a>
+				</aside>
 				<aside>2607 Lebanon Pike, Nashville, TN 37214</aside>
 			</div>
 			<div className="contact">
 				<div className="contact_text">
-					For more information or to place an order/commission, contact David at 615&#8209;885&#8209;1694
+					For more information or to place an order/commission, contact David at
+					615&#8209;885&#8209;1694
 				</div>
 				<div className="or">or</div>
 				<div className="contact_form">
-					<Input method={onChange} validate={() => null} label="Name" name="name" value={values.name}/>
-					<Input method={onChange} validate={validateEmail} is_valid={values.valid_email} label="Email" name="email" value={values.email}/>
-					<Input method={onChange} validate={() => null} label="Subject" name="subject" value={values.subject}/>
-					<TextArea method={onChange} label="Message" name="message" value={values.message}/>
-					<LargeButton disabled={values.valid_email} method={submitForm}>Submit</LargeButton>
-					{values.success && <SuccessToast/>}
+					<Input
+						method={onChange}
+						validate={() => null}
+						label="Name"
+						name="name"
+						value={values.name}
+					/>
+					<Input
+						method={onChange}
+						validate={validateEmail}
+						is_valid={values.valid_email}
+						label="Email"
+						name="email"
+						value={values.email}
+					/>
+					<Input
+						method={onChange}
+						validate={() => null}
+						label="Subject"
+						name="subject"
+						value={values.subject}
+					/>
+					<TextArea
+						method={onChange}
+						label="Message"
+						name="message"
+						value={values.message}
+					/>
+					<div className="g-recaptcha">
+						<ReCAPTCHA
+							onExpired={() => {
+								validateRecaptcha(false);
+							}}
+							onErrored={() => {
+								validateRecaptcha(false);
+							}}
+							sitekey="6Lc9m7IUAAAAAG9_dACBnAJk9oEgPwXUi3p7Wr2Y"
+							onChange={() => {
+								validateRecaptcha(true);
+							}}
+						/>
+					</div>
+					<LargeButton
+						disabled={values.valid_email && recaptchaStatus}
+						method={submitForm}
+					>
+						Submit
+					</LargeButton>
+					{values.success && <SuccessToast />}
 				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
